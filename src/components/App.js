48aec9adf0a54308b1b8e46ac39573dbca017787
bill <bill.arnold@student.moringaschool.com>
2023-10-25 19:35:15 +0300

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,71 @@
-import React from "react";
+// import React from "react";
 import Container from "./Container";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Container header="Example header" direction="horizontal">
+//         <div>This is an example!</div>
+//         <div>This is an example!</div>
+//         <div>This is an example!</div>
+//         <div>This is an example!</div>
+//         <div>This is an example!</div>
+//         <div>This is an example!</div>
+//         <div>This is an example!</div>
+//       </Container>
+//       <Container header="Example header" textPosition="center">
+//         <img
+//           alt="turtle"
+//           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+//         />
+//         <img
+//           alt="turtle"
+//           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+//         />
+//         <img
+//           alt="turtle"
+//           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+//         />
+//         <img
+//           alt="turtle"
+//           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+//         />
+//       </Container>
+//     </div>
+//   );
+// }
+
+import React from 'react';
+
+function Example(props) {
   return (
-    <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-      </Container>
-      <Container header="Example header" textPosition="center">
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-      </Container>
+    <div>
+      {/* Render a prop value */}
+      {props.exampleProp}
+
+      {/* Render children passed to the Example component */}
+      {props.children}
+    </div>
+  );
+}
+
+// Using the Example component with both props and children
+const App = () => {
+  return (
+    <div>
+      {/* Example with only a prop */}
+      <Example exampleProp="Example with prop only" />
+
+      {/* Example with both a prop and children */}
+      <Example exampleProp="Example with prop and children">
+        <h1>Example header!</h1>
+        <p>Some example text</p>
+      </Example>
     </div>
   );
 }
 
 export default App;
+
+
+
